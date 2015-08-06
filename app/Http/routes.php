4b8a8e3c36fc738b5ea2c7764c957ec6500e0b53
DimasRakhmad <dimasrakhmad@gmail.com>
2015-08-06 13:23:20 +0700

    update readme

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,41 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+# routing get hello
+Route::get('hello', function () {
+    return 'Hello World';
+});
+
+# routing match
+Route::match(['get', 'post'], 'match', function () {
+    return 'Hello ';
+});
+
+# routing any
+Route::any('foo', function () {
+    return 'Hello World';
+});
+
+#get id
+Route::get('user/{id}', function ($id) {
+    return 'User ' . $id;
+});
+
+#get name
+Route::get('name/{name?}', function ($name = "dimas") {
+    return $name;
+});
+
+#get where
+Route::get('coba/{id}/{name}', function ($id, $name) {
+    //
+})->where(['id' => '[0-9]+', 'name' => '[a-z]+']);
+
+Route::get('belajar', ['as' => 'belajar', function () {
+    return 'Hello World';
+}]);
+Route::get('baru', function () {
+    return view('baru');
+});
+
